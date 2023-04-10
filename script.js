a5e240d02ea6f6cd8aab66a53ee91b4904b5c0a1
xrscode <120644276+xrscode@users.noreply.github.com>
2023-04-10 09:10:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-//Tip Calculator
+const fruits = ["mango", "papaya", "pineapple", "apple"];
 
-const billTotal = 430;
-
-let tipTotal = (billTotal >= 50 && billTotal <= 300) ? 1.15 : 1.2;
-
-console.log(`The total cost of your food came to $${billTotal}.  The tip percentage for this value is: ${(billTotal >= 50 && billTotal <= 300) ? '1.15 %' : '1.20%'}.  Therefore your total bill comes to $${(billTotal * tipTotal)}.`);
+// Iterate over fruits below
+fruits.forEach((item) => console.log(`I want to eat a ${item}`));
