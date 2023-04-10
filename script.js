b169243034a86feb9dc5cbac21722bf54864abad
xrscode <120644276+xrscode@users.noreply.github.com>
2023-04-10 09:59:25 +0100

    Iterators
    Working with strings.

--- script.js
+++ script.js
@@ -2,3 +2,99 @@ const fruits = ["mango", "papaya", "pineapple", "apple"];
 
 // Iterate over fruits below
 fruits.forEach((item) => console.log(`I want to eat a ${item}`));
+
+const numbers = [1, 2, 3, 4, 5];
+
+const bigNumbers = numbers.map((number) => {
+  return number * 10;
+});
+
+console.log(numbers); // Output: [1, 2, 3, 4, 5]
+console.log(bigNumbers); // Output: [10, 20, 30, 40, 50]
+
+const animals = [
+  "Hen",
+  "elephant",
+  "llama",
+  "leopard",
+  "ostrich",
+  "Whale",
+  "octopus",
+  "rabbit",
+  "lion",
+  "dog",
+];
+
+// Create the secretMessage array below
+
+const secretMessage = animals.map((word) => {
+  return word[0];
+});
+
+console.log(secretMessage.join(""));
+const words = ["chair", "music", "pillow", "brick", "pen", "door"];
+
+const shortWords = words.filter((word) => {
+  return word.length < 6;
+});
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter((num) => {
+  return num < 250;
+});
+
+const favoriteWords = [
+  "nostalgia",
+  "hyperbole",
+  "fervent",
+  "esoteric",
+  "serene",
+];
+
+// Call .filter() on favoriteWords below
+const longFavoriteWords = favoriteWords.filter((word) => {
+  return word.length > 7;
+});
+
+const jumbledNums = [123, 25, 78, 5, 9];
+
+const lessThanTen = jumbledNums.findIndex((num) => {
+  return num < 10;
+});
+
+const animals = [
+  "hippo",
+  "tiger",
+  "lion",
+  "seal",
+  "cheetah",
+  "monkey",
+  "salamander",
+  "elephant",
+];
+
+const foundAnimal = animals.findIndex((animal) => {
+  return animal === "elephant";
+});
+
+const startsWithS = animals.findIndex((animal) => {
+  return animal[0] === "s";
+});
+
+const numbers = [1, 2, 4, 10];
+
+const summedNums = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+});
+
+console.log(summedNums); // Output: 17
+
+const numbers = [1, 2, 4, 10];
+
+const summedNums = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 100); // <- Second argument for .reduce()
+
+console.log(summedNums); // Output: 117
