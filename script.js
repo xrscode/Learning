96bb757b8a39afee6872d731581b706c65eee5f7
xrscode <120644276+xrscode@users.noreply.github.com>
2023-04-11 11:57:43 +0100

    Objects

--- script.js
+++ script.js
@@ -1,189 +1,101 @@
-let spaceship = {}; // spaceship is an empty object
+//
 
-"hello".length; // Returns 5
-
-let spaceship = {
-  homePlanet: "Earth",
-  color: "silver",
-};
-spaceship.homePlanet; // Returns 'Earth',
-spaceship.color; // Returns 'silver',
-
-["A", "B", "C"][0]; // Returns 'A'
-
-let spaceship = {
-  "Fuel Type": "Turbo Fuel",
-  "Active Duty": true,
-  homePlanet: "Earth",
-  numCrew: 5,
-};
-spaceship["Active Duty"]; // Returns true
-spaceship["Fuel Type"]; // Returns  'Turbo Fuel'
-spaceship["numCrew"]; // Returns 5
-spaceship["!!!!!!!!!!!!!!!"]; // Returns undefined
-
-let returnAnyProp = (objectName, propName) => objectName[propName];
-
-returnAnyProp(spaceship, "homePlanet"); // Returns 'Earth'
-
-const spaceship = { type: "shuttle" };
-spaceship = { type: "alien" }; // TypeError: Assignment to constant variable.
-spaceship.type = "alien"; // Changes the value of the type property
-spaceship.speed = "Mach 5"; // Creates a new key of 'speed' with a value of 'Mach 5'
-
-const spaceship = {
-  "Fuel Type": "Turbo Fuel",
-  homePlanet: "Earth",
-  mission: "Explore the universe",
-};
-
-delete spaceship.mission; // Removes the mission property
-
-const alienShip = {
-  invade: function () {
-    console.log(
-      "Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon."
-    );
+//Advanced Objects
+const goat = {
+  dietType: "herbivore",
+  makeSound() {
+    console.log("baaa");
   },
 };
 
-const alienShip = {
-  invade() {
-    console.log(
-      "Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon."
-    );
+goat.makeSound(); // Prints baaa
+
+const goat = {
+  dietType: "herbivore",
+  makeSound() {
+    console.log("baaa");
+  },
+  diet() {
+    console.log(dietType);
+  },
+};
+goat.diet();
+// Output will be "ReferenceError: dietType is not defined"
+
+const goat = {
+  dietType: "herbivore",
+  makeSound() {
+    console.log("baaa");
+  },
+  diet() {
+    console.log(this.dietType);
   },
 };
 
-alienShip.invade(); // Prints 'Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.'
+goat.diet();
+// Output: herbivore
 
-let retreatMessage =
-  "We no longer wish to conquer your planet. It is full of dogs, which we do not care for.";
-
-// Write your code below
-const alienShip = {
-  retreat() {
-    console.log(retreatMessage);
+const goat = {
+  dietType: "herbivore",
+  makeSound() {
+    console.log("baaa");
   },
-  takeOff() {
-    console.log("Spim... Borp... Glix... Blastoff!");
+  diet: () => {
+    console.log(this.dietType);
   },
 };
 
-alienShip.retreat();
-alienShip.takeOff();
+goat.diet(); // Prints undefined
 
-const spaceship = {
-  telescope: {
-    yearBuilt: 2018,
-    model: "91031-XLT",
-    focalLength: 2032,
-  },
-  crew: {
-    captain: {
-      name: "Sandra",
-      degree: "Computer Engineering",
-      encourageTeam() {
-        console.log("We got this!");
-      },
-    },
-  },
-  engine: {
-    model: "Nimbus2000",
-  },
-  nanoelectronics: {
-    computer: {
-      terabytes: 100,
-      monitors: "HD",
-    },
-    "back-up": {
-      battery: "Lithium",
-      terabytes: 50,
-    },
-  },
+const bankAccount = {
+  _amount: 1000,
 };
 
-spaceship.nanoelectronics["back-up"].battery; // Returns 'Lithium'
+bankAccount._amount = 1000000;
 
-let capFave = spaceship.crew.captain["favorite foods"][0];
-
-let spaceship = {
-  passengers: null,
-  telescope: {
-    yearBuilt: 2018,
-    model: "91031-XLT",
-    focalLength: 2032 
-  },
-  crew: {
-    captain: { 
-      name: 'Sandra', 
-      degree: 'Computer Engineering', 
-      encourageTeam() { console.log('We got this!') },
-     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
-  },
-
-
-  const spaceship = {
-    homePlanet : 'Earth',
-    color : 'silver'
-  };
-   
-  let paintIt = obj => {
-    obj.color = 'glorious gold'
-  };
-   
-  paintIt(spaceship);
-   
-  spaceship.color // Returns 'glorious gold'
-   
-  let spaceship = {
-    homePlanet : 'Earth',
-    color : 'red'
-  };
-  let tryReassignment = obj => {
-    obj = {
-      identified : false, 
-      'transport type' : 'flying'
+const person = {
+  _firstName: "John",
+  _lastName: "Doe",
+  get fullName() {
+    if (this._firstName && this._lastName) {
+      return `${this._firstName} ${this._lastName}`;
+    } else {
+      return "Missing a first name or a last name.";
     }
-    console.log(obj) // Prints {'identified': false, 'transport type': 'flying'}
-   
-  };
-  tryReassignment(spaceship) // The attempt at reassignment does not work.
-  spaceship // Still returns {homePlanet : 'Earth', color : 'red'};
-   
-  spaceship = {
-    identified : false, 
-    'transport type': 'flying'
-  }; // Regular reassignment still works.
+  },
+};
 
+// To call the getter method:
+person.fullName; // 'John Doe'
 
-
-  let spaceship = {
-    crew: {
-      captain: { 
-        name: 'Lily', 
-        degree: 'Computer Engineering', 
-        cheerTeam() { console.log('You got this!') } 
-      },
-      'chief officer': { 
-        name: 'Dan', 
-        degree: 'Aerospace Engineering', 
-        agree() { console.log('I agree, captain!') } 
-      },
-      medic: { 
-        name: 'Clementine', 
-        degree: 'Physics', 
-        announce() { console.log(`Jets on!`) } },
-      translator: {
-        name: 'Shauna', 
-        degree: 'Conservation Science', 
-        powerFuel() { console.log('The tank is full!') } 
-      }
+const person = {
+  _age: 37,
+  set age(newAge) {
+    if (typeof newAge === "number") {
+      this._age = newAge;
+    } else {
+      console.log("You must assign a number to age");
     }
-  }; 
-   
-  // for...in
-  for (let crewMember in spaceship.crew) {
-    console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`);
-  }
-  
+  },
+};
+
+person.age = 40;
+console.log(person._age); // Logs: 40
+person.age = "40"; // Logs: You must assign a number to age
+
+person._age = "forty-five";
+console.log(person._age); // Prints forty-five
+
+const monsterFactory = (name, age, energySource, catchPhrase) => {
+  return {
+    name: name,
+    age: age,
+    energySource: energySource,
+    scare() {
+      console.log(catchPhrase);
+    },
+  };
+};
+
+const ghost = monsterFactory("Ghouly", 251, "ectoplasm", "BOO!");
+ghost.scare(); // 'BOO!'
