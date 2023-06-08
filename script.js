53d34df99d4b3e5ddeff146b694fb9e685c3680f
xrscode <120644276+xrscode@users.noreply.github.com>
2023-06-08 10:15:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-const fruits = ["mango", "papaya", "pineapple", "apple"];
+function findTicketPrices(emailString) {
+  let regex = /\d+/;
+  return regex.test(emailString);
+}
 
-// Iterate over fruits below
-fruits.forEach((item) => console.log(`I want to eat a ${item}`));
+console.log(
+  findTicketPrices("Hello, I think I can pay 11 pounds, is that alright?")
+);
+
+const tree = {
+  name: "Oak",
+  hasAcorns: true,
+  ageInYears: 530,
+};
+
+for (const key in tree) {
+  // code to be executed
+}
+
+for (const key in tree) {
+  const value = tree[key];
+  console.log(`The tree has a key of ${key} holding the value ${value}`);
+  // The tree has a key of name holding the value Oak
+  // The tree has a key of hasAcorns holding the value true
+  // The tree has a key of ageInYears holding the value 530
+}
