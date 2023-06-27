b5f41edaeea41091ef648060eb3919b00aa593e5
xrscode <120644276+xrscode@users.noreply.github.com>
2023-06-27 10:01:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,53 @@
 //     .join("");
 // }
 // console.log(reverseLetter("krish21an"));
+
+//Row Weights - KYU 7:
+// https://www.codewars.com/kata/5abd66a5ccfd1130b30000a9/train/javascript
+// function rowWeights(array) {
+//   //your code here
+//   const myArr = [0, 0];
+//   array.forEach(function (value, index) {
+//     index % 2 === 0 ? (myArr[0] += value) : (myArr[1] += value);
+//   });
+//   return myArr;
+// }
+
+//Deodorant Evaporator - KYU 7:
+// https://www.codewars.com/kata/5506b230a11c0aeab3000c1f/train/javascript
+// function evaporator(content, evap_per_day, threshold) {
+//   let num = 0;
+//   const min = (content / 100) * threshold;
+//   while (content > min) {
+//     num += 1;
+//     content -= (content / 100) * evap_per_day;
+//   }
+//   return num;
+// }
+// console.log(evaporator(10, 10, 5));
+
+//Data Reverse - 6kyu:
+//https://www.codewars.com/kata/569d488d61b812a0f7000015/train/javascript
+function dataReverse(data) {
+  // Your code here
+  const myArr = [];
+  const group = 8;
+  for (let i = 0; i < data.length; i += group) {
+    const eights = data.slice(i, i + group);
+    myArr.push(eights);
+  }
+  const myArr2 = [];
+  myArr.reverse().forEach(function (value) {
+    value.forEach(function (val2) {
+      myArr2.push(val2);
+    });
+  });
+
+  return myArr2;
+}
+console.log(
+  dataReverse([
+    1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+    0, 1, 0, 1, 0, 1, 0,
+  ])
+);
