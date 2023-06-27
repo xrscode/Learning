e000c7811a8abbe010e8a525bbf3bd7fa5c5414f
xrscode <120644276+xrscode@users.noreply.github.com>
2023-06-27 09:12:16 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+//Reverse Letter - KYU 7:
+// https://www.codewars.com/kata/58b8c94b7df3f116eb00005b/solutions/javascript
+// function reverseLetter(str) {
+//   return str
+//     .split("")
+//     .reverse()
+//     .filter(function (char) {
+//       return /[^a-zA-Z]/.test(char);
+//     })
+//     .join("");
+// }
+// console.log(reverseLetter("krish21an"));
