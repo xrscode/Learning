e502e0750f1b5c2201f6b918cc3edc4e1996e35a
xrscode <120644276+xrscode@users.noreply.github.com>
2023-08-22 10:32:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -150,3 +150,28 @@ const bookEZY23 = book.bind(easyJet, 23);
 console.log(bookEZY23("Bean")); //Bean booked a seat on Easy Jet, flight number: EZY23;
 
 //With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+document
+  .querySelector(".buy")
+  .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+
+//More Partial Application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.2);
+// addVAT = value => value + value * 0.2;
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(23));
+console.log(addVAT2(100));
