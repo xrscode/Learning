a4a1557a203220aae42b493b8b52b2f8cd01b72a
xrscode <120644276+xrscode@users.noreply.github.com>
2023-11-02 09:24:50 +0000

    Update script.js

--- script.js
+++ script.js
@@ -143,3 +143,90 @@ const numDeposits1000 = accounts
     return b >= 1000 ? a + 1 : a;
   }, 0);
 console.log(numDeposits1000); //Returns 6.
+
+//Exercise 3.
+//Create new Object with all deposits.
+const sums = accounts
+  .flatMap((acc) => acc.movements)
+  .reduce((object, element, index) => {
+    element >= 1000 ? (object[index] = element) : object;
+    return object;
+  }, {});
+console.log(sums); //{3: 3000, 7: 1300, 8: 5000, 9: 3400, 14: 8500, 25: 1000}
+
+//Create Object with reduced Deposits and Withdrawals.
+const sums2 = accounts
+  .flatMap((acc) => acc.movements)
+  .reduce(
+    (object, element) => {
+      object[element > 0 ? "Deposits" : "Withdrawals"] += element;
+      return object;
+    },
+    { Deposits: 0, Withdrawals: 0 }
+  );
+console.log(sums2); //{Deposits: 25180, Withdrawals: -7340}
+
+//Exercise 4.  Turn Any string into Title Case.
+const convertTitle = function (string) {
+  const exceptions = ["a", "b", "h"];
+  return string
+    .split(" ")
+    .map((x) => {
+      return exceptions.includes(x[0].toLowerCase())
+        ? x.toLowerCase()
+        : x[0].toUpperCase() + x.slice(1);
+    })
+    .join(" ");
+};
+console.log(convertTitle("hello dylan, you are a dog!"));
+console.log("CODING CHALLENGE 4");
+/*
+Julia & Kate are studying dogs.  Do the dogs eat too much, or too little?  An acceptible amount to eat is if the current food portion is within range 10% above and 10% below the recommended portion. 
+
+Step 1. Loop over the array containing dob objects, and for each dog, calculate the recommended food portion and add it to the object as a new property.  Do Not create a new array, simply loop over the array.  
+Formula: recommendedFood = weight ** 0.75 * 28.  (Result in grams).  MUST BE IN KG.
+
+Step 2. Find Sarah's dog and log to the console whether he has eaten too much or too little. Note: Some dogs have multiple owners. 
+
+Step 3.  Create an Array containing all owners of dogs who eat too much: 'ownersEatTooMuch'.   Create an Array with all owners of dogs who eat too little; 'ownersEatTooLittle'.
+
+Step 4. Log a string to the console for each array created in 3., for example; 'Matilida and Alice and Bob's dogs eat too much!.  And... 'Sarah and John and Michael's dogs eat too little!'
+
+Step 5.  Log to the console whether there is any dog eating the correct amount of food.  Just log true or false. .some() ?
+
+Step 6. Log to the console whether there are any dogs eating an acceptable amount of food (true or false)
+
+Step 7.  Create an array containing the dogs that are eating an OK amount of food. 
+
+Step 8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order.  
+*/
+
+//Test Data:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ["Alice", "Bob"] },
+  { weight: 8, curFood: 200, owners: ["Matilda"] },
+  { weight: 13, curFood: 275, owners: ["Sarah", "John"] },
+  { weight: 32, curFood: 340, owners: ["Michael"] },
+];
+
+//Step 1.
+dogs.forEach(
+  (x) => (x.recommendedFood = Math.round(x.weight ** 0.75 * 28) / 100)
+);
+console.log(dogs);
+
+//Step 2.
+//Sarahs Dog.
+const sarahDog = dogs.find((x) => x.owners.includes("Sarah"));
+console.log(
+  `Sarahs dog is eating: ${
+    Math.round(sarahDog.curFood) / 100
+  }kg.  The amount her dog should eat is: ${sarahDog.recommendedFood}kg`
+);
+if (Math.round(sarahDog.curFood) / 100 >= sarahDog.recommendedFood * 1.1) {
+  console.log("Too much");
+}
+if (Math.round(sarahDog.curFood) / 100 <= sarahDog.recommendedFood / 1.1) {
+  console.log("Too little");
+}
+if()
